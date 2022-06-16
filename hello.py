86415d0ce2aaabb0d4f0b3a683ce4ea038208a9a
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-06-15 22:28:36 -0500

    Adding hello program with input

---
+++ hello.py
@@ -0,0 +1,9 @@
+#This program says hello and asks for my name
+
+print("Hello world!")
+print("What is your name?")
+myName = input()
+print(f'It is good to meet you, {myName}')
+print(f'The length of your name is: {len(myName)}')
+my_age = input()
+print(f'You will be {str(int(my_age) + 1)} in a year')
